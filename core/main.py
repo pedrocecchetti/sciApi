cf61710ee06ac0db1068cab85ae5ff47c714226c
Cecchetti <pedro.cecchetti@gmail.com>
2020-08-24 00:19:16 +0200

    feat: Added routes to read and create scientist

--- core/main.py
+++ core/main.py
@@ -18,4 +18,21 @@ def get_db():
 
 @app.get("/")
 async def index():
-    return {"message": "Welcome to SciAPI!"}
+    return {"Message": "Welcome to The SciAPI"}
+
+
+@app.get("/scientist/{first_name}", response_model=schema.Scientist)
+async def read_scientist_by_first_name(first_name: str, db: Session = Depends(get_db)):
+    scientist = crud.get_scientist_by_first_name(db, first_name=first_name)
+    if scientist is None:
+        raise HTTPException(status_code=404, detail="Scientist Not Found!")
+    return scientist
+
+
+@app.post("/scientist/", response_model=schema.Scientist)
+async def create_scientist(scientist: schema.ScientistCreate, db: Session = Depends(get_db)):
+    full_name = scientist.first_name + " " + scientist.last_name
+    scientist_db = crud.get_scientist_by_full_name(db, full_name=full_name)
+    if scientist_db:
+        raise HTTPException(status_code=400, detail="This Scientist already exists!")
+    return crud.create_scientist(db=db, scientist=scientist)
