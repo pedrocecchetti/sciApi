1ce4ebd7f2bc6fa70d6f143fe1f0f86d6b996a32
Cecchetti <pedro.cecchetti@gmail.com>
2020-08-24 22:01:06 +0200

    Fixing Git mess!

--- core/main.py
+++ core/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
+from typing import List
 from sqlalchemy.orm import Session
 
 from .database import crud, schema
 from .database.db import SessionLocal
+from .routers.scientists import router
 
 app = FastAPI()
 
@@ -15,25 +17,9 @@ def get_db():
     finally:
         db.close()
 
-
 @app.get("/")
 async def index():
     return {"Message": "Welcome to The SciAPI"}
 
+app.include_router(router, prefix="/scientist")
 
-@app.get("/scientist/{first_name}", response_model=schema.Scientist)
-async def read_scientist_by_first_name(first_name: str, db: Session = Depends(get_db)):
-    treated_first_name = first_name.lower().capitalize()
-    scientist = crud.get_scientist_by_first_name(db, first_name=treated_first_name  )
-    if scientist is None:
-        raise HTTPException(status_code=404, detail="Scientist Not Found!")
-    return scientist
-
-
-@app.post("/scientist/", response_model=schema.Scientist)
-async def create_scientist(scientist: schema.ScientistCreate, db: Session = Depends(get_db)):
-    full_name = scientist.first_name + " " + scientist.last_name
-    scientist_db = crud.get_scientist_by_full_name(db, full_name=full_name)
-    if scientist_db:
-        raise HTTPException(status_code=400, detail="This Scientist already exists!")
-    return crud.create_scientist(db=db, scientist=scientist)
